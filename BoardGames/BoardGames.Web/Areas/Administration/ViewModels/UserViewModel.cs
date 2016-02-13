a61d19fbd913ccd7bab1ca62e2a0ad079c781ff0
RYordanova <rodoteia.yordanova@gmail.com>
2016-02-13 13:39:34 +0200

    Added admin panel

--- BoardGames/BoardGames.Web/Areas/Administration/ViewModels/UserViewModel.cs
+++ BoardGames/BoardGames.Web/Areas/Administration/ViewModels/UserViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using AutoMapper;
 using BoardGames.Models;
 using BoardGames.Web.Infrastructure.Mapping;
@@ -10,14 +11,19 @@ namespace BoardGames.Web.Areas.Administration.ViewModels
 {
     public class UserViewModel : IMapFrom<User>, IHaveCustomMappings
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Username { get; set; }
+        public string Email { get; set; }
+
+        //public string Role { get; set; }
 
         public void CreateMappings(IConfiguration configuration)
         {
             configuration.CreateMap<User, UserViewModel>("Username")
                 .ForMember(m => m.Username, opt => opt.MapFrom(t => t.UserName))
+                .ForMember(m => m.Email, opt => opt.MapFrom(t => t.Email))
+                //.ForMember(m => m.Role, opt => opt.MapFrom(t => Roles.Where(r => user.Roles.Select(ur => ur.RoleId).Contains(r.Id)).Select(r => r.Name)))
                 .ReverseMap();
         }
     }
