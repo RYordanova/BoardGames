40ab3ef068f7eb6d504b3552ba2a9aee73605423
RYordanova <rodoteia.yordanova@gmail.com>
2016-02-23 00:00:25 +0200

    Delete unnecessary methods in chat class

--- BoardGames/BoardGames.Web/Hubs/Chat.cs
+++ BoardGames/BoardGames.Web/Hubs/Chat.cs
@@ -9,21 +9,5 @@ namespace BoardGames.Web.Hubs
             var msg = string.Format("{0}: {1}", Context.ConnectionId, message);
             Clients.All.addMessage(msg);
         }
-
-        public void JoinRoom(string room)
-        {
-            Groups.Add(Context.ConnectionId, room);
-            Clients.Caller.joinRoom(room);
-        }
-
-        public void SendMessageToRoom(string message, string[] rooms)
-        {
-            var msg = string.Format("{0}: {1}", Context.ConnectionId, message);
-
-            for (int i = 0; i < rooms.Length; i++)
-            {
-                Clients.Group(rooms[i]).addMessage(msg);
-            }
-        }
     }
 }
